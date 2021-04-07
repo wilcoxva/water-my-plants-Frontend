a47a79b00ff229997a371e46bd5dd9758fb561cc
Virginia Wilcox <wilcoxva@gmail.com>
2021-04-07 11:52:00 -0400

    cleaned up nav css

--- water-my-plants/src/components/Navigation.js
+++ water-my-plants/src/components/Navigation.js
@@ -7,19 +7,18 @@ const Navigation = ({isLoggedIn}) => {
     <div>
       <div className="App">
         <div className="ncontainer">
-          <Link to="/" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>Home</Link>
-          <Link to="/about" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>About</Link>
-          <Link to="/contact" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>Contact</Link>
-        </div>
-        <div className="ncontainer">
-          <Link to="/PlantsList" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>PlantsList</Link>
-        </div>
-        <div className="ncontainer">
-          {isLoggedIn ?
-            <Link to="/LogOut" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>LogOut</Link> :
-            [<Link to="/LogIn" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>LogIn</Link>,
-            <Link to="/SignUp" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>SignUp</Link>]
-          }
+          <h2>Water My Plants</h2>
+          <div className="links">
+            <Link to="/" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>Home</Link>
+            <Link to="/about" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>About</Link>
+            <Link to="/contact" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>Contact</Link>
+            <Link to="/PlantsList" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>PlantsList</Link>
+            {isLoggedIn ?
+              <Link to="/LogOut" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>LogOut</Link> :
+              [<Link to="/LogIn" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>LogIn</Link>,
+              <Link to="/SignUp" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>SignUp</Link>]
+            }
+          </div>
         </div>
       </div>
     </div>
