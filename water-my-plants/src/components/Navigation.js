737d0bd7580898c836de0e3d31a2dda877d436b3
Virginia Wilcox <wilcoxva@gmail.com>
2021-04-08 10:32:34 -0400

    changed nav to not show plantslist if not logged in

--- water-my-plants/src/components/Navigation.js
+++ water-my-plants/src/components/Navigation.js
@@ -12,7 +12,10 @@ const Navigation = ({isLoggedIn}) => {
             <Link to="/" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>Home</Link>
             <Link to="/about" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>About</Link>
             <Link to="/contact" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>Contact</Link>
-            <Link to="/PlantsList" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>PlantsList</Link>
+            {isLoggedIn ?
+              <Link to="/PlantsList" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>PlantsList</Link> :
+              <Link />
+            }
             {isLoggedIn ?
               <Link to="/LogOut" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>LogOut</Link> :
               [<Link to="/LogIn" style={{ textDecoration: 'none', color: "gray", margin: "5px", padding: "5px" }}>LogIn</Link>,
