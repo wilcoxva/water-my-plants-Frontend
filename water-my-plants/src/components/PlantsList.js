ef2dc66e5a64c0e3fa9d00b5402b702b9ce5916f
Virginia Wilcox <wilcoxva@gmail.com>
2021-04-12 11:42:25 -0400

    used display flex on an outer container for plantslist page

--- water-my-plants/src/components/PlantsList.js
+++ water-my-plants/src/components/PlantsList.js
@@ -45,28 +45,31 @@ const PlantsList = (props) => {
     return (
         <div className="alt-bg">
             <Navigation isLoggedIn={isLoggedIn} />
-            <div className="plantslist_form">
-                <h1>Create a new plant:</h1>
-                <form onSubmit={submitHandler}>
-                    <input type="text" name="nickname" placeholder="Enter your nickname." value={info.nickname} onChange={handleChange}/><br/>
-                    <input type="text" name="common_name" placeholder="Common name." value={info.common_name} onChange={handleChange}/><br/>
-                    <input type="text" name="h2oFrequency" placeholder="H2o frequency." value={info.h2oFrequency} onChange={handleChange}/><br/>
-                    <input type="text" name="image_url" placeholder="Image url." value={info.image_url} onChange={handleChange}/><br/>
-                    <button type="submit">Submit</button>
-                </form>   
+            <div className="pl-outer-container">
+                <div className="plantslist_form">
+                    <h1>Create a new plant:</h1>
+                    <form onSubmit={submitHandler}>
+                        <input type="text" name="nickname" placeholder="Enter your nickname." value={info.nickname} onChange={handleChange}/><br/>
+                        <input type="text" name="common_name" placeholder="Common name." value={info.common_name} onChange={handleChange}/><br/>
+                        <input type="text" name="h2oFrequency" placeholder="H2o frequency." value={info.h2oFrequency} onChange={handleChange}/><br/>
+                        <input type="text" name="image_url" placeholder="Image url." value={info.image_url} onChange={handleChange}/><br/>
+                        <button type="submit">Submit</button>
+                    </form>   
+                </div>
+                <div className="pl-container">
+                    {plants.map(plant => (
+                        <div className="p-container" key={plant.nickname}>
+                            <h1>{plant.nickname}</h1>
+                            <p>Common name: {plant.common_name}</p>
+                            <p>Thirst level: {plant.h2oFrequency}</p>
+                            <img src={plant.image_url} alt="Plant" width="200" height="150" />
+                            <p>{user.username}'s Plant</p>
+                            <Link to={{ pathname:`/${plant.user_id}/${plant.id}` }}>View Me</Link>
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className="pl-container">
-                {plants.map(plant => (
-                    <div className="p-container" key={plant.nickname}>
-                        <h1>{plant.nickname}</h1>
-                        <p>Common name: {plant.common_name}</p>
-                        <p>Thirst level: {plant.h2oFrequency}</p>
-                        <img src={plant.image_url} alt="Plant" width="200" height="150" />
-                        <p>{user.username}'s Plant</p>
-                        <Link to={{ pathname:`/${plant.user_id}/${plant.id}` }}>View Me</Link>
-                    </div>
-                ))}
-            </div>
+            
         </div>
     );
 };
